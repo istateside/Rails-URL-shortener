e337b016223141894928c1ef8ae85a94de107dea
Kevin Fleischman <kevin.r.fleischman@gmail.com>
2014-09-24 15:20:45 -0400

    initial commit begin making modles

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,6 @@
+require 'securerandom'
+class ShortenedUrl < ActiveRecord::Base
+  validates :long_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
+  validates :submitter_id, presence: true
+end
